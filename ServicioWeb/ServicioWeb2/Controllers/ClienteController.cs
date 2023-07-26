d8a2df5453a641dcf2587e9a232f99bfe501e56f
Franco Guardiani <franguardiani08@gmail.com>
2023-07-26 16:24:59 -0300

    FRanco

--- ServicioWeb/ServicioWeb2/Controllers/ClienteController.cs
+++ ServicioWeb/ServicioWeb2/Controllers/ClienteController.cs
@@ -4,31 +4,41 @@ using ServicioWeb2.Models;
 using ServicioWeb2.Models.Response;
 using ServicioWeb2.Models.Request;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
 
 
 namespace ServicioWeb2.Controllers
 {
-  [Route("api/[controller]")]
+  [EnableCors("ReglasCors")]
+  [Route("api/cliente")]
   [ApiController]
   public class ClienteController : ControllerBase
   {
+    //Se crea un objeto del tipo contexto de la base de datos
     public readonly VentasRealContext _dbcontext;
 
     public ClienteController(VentasRealContext _context)
     {
+      //En el constructor se le da los atributos
       _dbcontext = _context;
     }
 
+
     [HttpGet]
-    [Route("Lista")]
+    [Route("lista")]
     public IActionResult Get()
     {
+      //Creo un objeto tipo respuesta donde se guarda el estado de la solicitud (1 o 0)
+      //un mensaje y la data si se trata de un get
       Respuesta oRespuesta = new Respuesta();
       try
       {
+        //Creo un entorno y uso el contexto de la base de datos para comunicarme con las tablas
         using (_dbcontext)
         {
+          //Realizo la query
           var lt = _dbcontext.Clientes.ToList();
+          //Si la operacion termina bien se cambia el estado de la solicitud y se carga los objetos obtenidos en la query a Data
           oRespuesta.Exito = 1;
           oRespuesta.Data = lt;
         }
@@ -40,8 +50,35 @@ namespace ServicioWeb2.Controllers
       return Ok(oRespuesta);
     }
 
+
+    [HttpGet]
+    [Route("obtener/{id:long}")]
+    //Para obtener un solo cliente
+    public IActionResult GetCliente(long id)
+    {
+      Respuesta oRespuesta = new Respuesta();
+      try
+      {
+        using (_dbcontext)
+        {
+          
+          oRespuesta.Data = _dbcontext.Clientes.Find(id);
+          if(oRespuesta.Data == null) return Ok(oRespuesta);
+
+          oRespuesta.Exito = 1;
+        }
+      }
+      catch (Exception ex)
+      {
+        oRespuesta.Mensaje = ex.Message;
+      }
+
+      return Ok(oRespuesta);
+    }
+
+
     [HttpPost]
-    [Route("Nuevo")]
+    [Route("nuevo")]
     public IActionResult Post(ClienteRequest oModel)
     {
       Respuesta oRespuesta = new Respuesta();
@@ -67,8 +104,9 @@ namespace ServicioWeb2.Controllers
       return Ok(oRespuesta);
     }
 
+
     [HttpPut]
-    [Route("Editar")]
+    [Route("editar")]
     public IActionResult Edit(ClienteRequest oModel)
     {
       Respuesta oRespuesta = new Respuesta();
@@ -77,10 +115,43 @@ namespace ServicioWeb2.Controllers
         using (_dbcontext)
         {
           Cliente oCliente = _dbcontext.Clientes.Find(oModel.Id);
-          oCliente.Nombre = oModel.Nombre;
-          oCliente.Apellido = oModel.Apellido;
 
-          _dbcontext.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+          if (oCliente == null) return Ok(oRespuesta);
+
+          //Por si vienen valores nulos se deja el que ya tenia
+          oCliente.Nombre = oModel.Nombre is null ? oCliente.Nombre : oModel.Nombre;
+          oCliente.Apellido = oModel.Apellido is null ? oCliente.Apellido: oModel.Apellido;
+
+          //_dbcontext.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+          _dbcontext.Clientes.Update(oCliente);
+          _dbcontext.SaveChanges();
+
+          oRespuesta.Exito = 1;
+        }
+      }
+      catch (Exception ex)
+      {
+        oRespuesta.Mensaje = ex.Message;
+      }
+
+      return Ok(oRespuesta);
+    }
+
+
+    [HttpDelete]
+    [Route("eliminar/{id:long}")]
+    public IActionResult Delete(long id)
+    {
+      Respuesta oRespuesta = new Respuesta();
+      try
+      {
+        using (_dbcontext)
+        {
+          Cliente oCliente = _dbcontext.Clientes.Find(id);
+
+          if (oCliente == null) return Ok(oRespuesta);
+
+          _dbcontext.Clientes.Remove(oCliente);
           _dbcontext.SaveChanges();
 
           oRespuesta.Exito = 1;
--- ServicioWeb/ServicioWeb2/Models/Response/Respuesta.cs
+++ ServicioWeb/ServicioWeb2/Models/Response/Respuesta.cs
@@ -3,8 +3,8 @@ namespace ServicioWeb2.Models.Response
   public class Respuesta
   {
     public int Exito { get; set; }
-    public string Mensaje { get; set; }
-    public object Data { get; set; }
+    public string Mensaje { get; set; } = null!;
+    public object Data { get; set; } = null!;
 
     public Respuesta() => this.Exito= 0;
   }
